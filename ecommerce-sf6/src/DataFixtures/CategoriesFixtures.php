e59bf1b091fb5dd81b6a13d11285b686fa0b5597
khalidzahiri <virtues01seasons@icloud.com>
2023-03-18 12:19:49 +0100

    V2 modifications de fixture Categories

--- ecommerce-sf6/src/DataFixtures/CategoriesFixtures.php
+++ ecommerce-sf6/src/DataFixtures/CategoriesFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Categories;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use function PHPUnit\Framework\returnCallback;
 
 class CategoriesFixtures extends Fixture
 {
@@ -13,23 +14,22 @@ class CategoriesFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-         $parent = new Categories();
-         $parent ->setName('Informatique');
-         $parent ->setSlug($this->slugger->slug($parent->getName())->lower());
-         $manager ->persist($parent);
+         $parent = $this->createCategory('Informatique',null,$manager );
 
-        $category = new Categories();
-        $category->setName('Ordinateurs portables');
-        $category->setSlug($this->slugger->slug($category->getName())->lower());
-        $category->setParent($parent);
-        $manager->persist($category);
-
-        $category = new Categories();
-        $category->setName('Ecran');
-        $category->setSlug($this->slugger->slug($category->getName())->lower());
-        $category->setParent($parent);
-        $manager->persist($category);
+         $this->createCategory('Ordinateurs portables', $parent, $manager);
+         $this->createCategory('Ecrans', $parent, $manager);
+         $this->createCategory('Souris', $parent, $manager);
 
         $manager->flush();
+    }
+    public function createCategory(string $name, Categories $parent =null, ObjectManager $manager )
+    {
+        $category = new Categories();
+        $category->setName($name);
+        $category->setSlug($this->slugger->slug($category->getName())->lower());
+        $category->setParent($parent);
+        $manager->persist($category);
+
+        return $category;
     }
 }
