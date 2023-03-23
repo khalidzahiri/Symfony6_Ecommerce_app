d0e982128095423548b41222ac3858e6d3855b44
khalidzahiri <virtues01seasons@icloud.com>
2023-03-23 05:56:26 +0100

    modification de l'entité users et creation du column restToken au niveau de l'entité Users

--- ecommerce-sf6/src/Entity/Users.php
+++ ecommerce-sf6/src/Entity/Users.php
@@ -53,6 +53,9 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private ?bool $is_verified = false;
 
+    #[ORM\Column(type: 'string', length: 100)]
+    private ?string $resetToken;
+
     #[ORM\OneToMany(mappedBy: 'users', targetEntity: Orders::class)]
     private Collection $orders;
 
@@ -202,6 +205,15 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
         $this->is_verified = $is_verified;
 
         return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->resetToken;
+    }
+    public function setResetToken(?string $resetToken): self
+    {
+        $this->resetToken = $resetToken;
     }
 
     /**
